8ca0ec580dcca2e9adbcf6f8c022445db7228fa7
krsgarland <97922634+krsgarland@users.noreply.github.com>
2022-05-10 14:50:59 -0500

    .

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const { get } = require("http");
 
 const app = express();
 
 app.use(express.static("./dist/gatefoldr"));
 
-app/get("/*", (_res) =>
- resizeBy.sendFile("index.html", {root: "dist/gatefoldr"})
+app.get("/*", (_,res) =>
+ res.sendFile("index.html", {root: "dist/gatefoldr"})
  );
 
  app.listen(process.env.PORT || 8080);
