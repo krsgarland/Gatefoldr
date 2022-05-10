49095bd581d471d68ae3fbe817c9e65ee3569001
krsgarland <97922634+krsgarland@users.noreply.github.com>
2022-05-10 14:13:50 -0500

    Deploy-Practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyCollectionComponent } from './my-collection/my-collection.component';
+import { RecordDetailsComponent } from './record-details/record-details.component';
+import { RecordListComponent } from './record-list/record-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingComponent } from './app-routing/app-routing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCollectionComponent,
+    RecordDetailsComponent,
+    RecordListComponent,
+    NavbarComponent,
+    DashboardComponent,
+    AppRoutingComponent,
   ],
   imports: [
     BrowserModule
